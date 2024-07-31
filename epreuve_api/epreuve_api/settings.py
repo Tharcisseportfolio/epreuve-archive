2ead609da2ea778f0a58eb7ae1ee5776cd54d74d
tharcissentirandekura <rest.apily@gmail.com>
2024-07-31 08:39:21 +0000

    save changes for adding staticfiles storage in settings.py

--- epreuve_api/epreuve_api/settings.py
+++ epreuve_api/epreuve_api/settings.py
@@ -139,6 +139,7 @@ STATIC_URL = 'static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
